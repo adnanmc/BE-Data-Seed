27447ec95f805105a780857bc94f1500466aa027
adnanmc <adnanmcj@gmail.com>
2018-07-06 15:31:55 -0400

    refactor code: filter only when file is found

--- filter.py
+++ filter.py
@@ -69,7 +69,7 @@ def call_adhoc_4_and_check_for_response_csv(receive_folder, send_folder, local_d
       request_file.write(f'ADH004_{current_date}')
       adh004String = f'ADH004_{current_date}'
       request_file.close()
-      print(f' Sending adhoc 4 request using: {adh004String}')
+      print(f' Sending adhoc 4 request using: {adh004String} and waiting for file....')
       time.sleep(20)
     # check if the file exist
     receivedFiles = os.listdir(receive_folder)
@@ -78,7 +78,6 @@ def call_adhoc_4_and_check_for_response_csv(receive_folder, send_folder, local_d
     # if fnmatch.fnmatch(file, pattern) == True:
     if fnmatch.filter(receivedFiles, receivePattern):
         file_name = fnmatch.filter(receivedFiles, receivePattern)
-        print(file_name[0])
         print(f'csv file for adhoc 4 found')
         # Copy file
         shutil.copy(f'{receive_folder}/{file_name[0]}', f'{local_directory}/DATA/data.csv')
@@ -171,6 +170,159 @@ def call_adhoc_4_and_check_for_response_csv(receive_folder, send_folder, local_d
         with open(f'{local_directory}/DATA/data.json', 'w') as f:
             json.dump(rows, f)
 
+        with open('DATA/data.json') as f:
+            data = json.load(f)
+            filteredData = []
+            for x in data:
+                if (x['STDudt'].strip() < x['ETDudt'].strip() 
+                and x['OUTudt'].strip() != '' 
+                and x['OFFudt'].strip() != ''
+                and x['ONudt'].strip() != ''
+                and x['INudt'].strip() != ''
+                and x['tailNumber'].strip().startswith('-') == False):
+                    filteredData.append(x)
+
+
+            with open('filtered_data/filtered.csv', 'w+', newline='') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(['recordStatus',
+                                'lastDateModified',
+                                'lastTimeModified',
+                                'lastUserToModify',
+                                'legDepartureDate',
+                                'airlineCode',
+                                'identifier',
+                                'sequence',
+                                'flightOriginDay',
+                                'numericFlightDate',
+                                'numGMTDate',
+                                'STDudt',
+                                'STAudt',
+                                'tailNumber',
+                                'numLastDateModified',
+                                'flightStatus',
+                                'origin',
+                                'STDLocal',
+                                'dispatchDesk',
+                                'STDGMTVariance',
+                                'destination',
+                                'STALocal',
+                                'STAGMTVariance',
+                                'OAGEquipmentType',
+                                'ACConfiguration',
+                                'serviceType',
+                                'originGate',
+                                'ETDlocal',
+                                'ETDudt',
+                                'TAXIutc',
+                                'OUTudt',
+                                'OFFudt',
+                                'destinationGate',
+                                'ETAlocal',
+                                'ETAudt',
+                                'ONudt',
+                                'INudt',
+                                'previousTailNumber',
+                                'ETE',
+                                'DCNutc',
+                                'ETOutc',
+                                'EONutc',
+                                'EDTCutc',
+                                'flightType',
+                                'newDepartureCity',
+                                'newArrivalCity',
+                                'SchedOAGEquipType',
+                                'OAGEquipSubType',
+                                'csvFSDailyID',
+                                'tailNumBeforeCancel',
+                                'CTAUTC',
+                                'cancelled',
+                                'replaced',
+                                'ATCStatus',
+                                'scheduledFlightType',
+                                'aircraftRouting',
+                                'mealService',
+                                'hub',
+                                'landingRestriction',
+                                'headStartFlight',
+                                'actualDeparture',
+                                'specialFlight',
+                                'actualArrival',
+                                'scheduledTaxiOut',
+                                'scheduledTaxiIn',
+                                'STOSetByUser',
+                                'STISetByUser',
+                                'CTFlightNumber'])
+                for flight in filteredData:
+                    writer.writerow([flight['recordStatus'],
+                                    flight['lastDateModified'],
+                                    flight['lastTimeModified'],
+                                    flight['lastUserToModify'],
+                                    flight['legDepartureDate'],
+                                    flight['airlineCode'],
+                                    flight['identifier'],
+                                    flight['sequence'],
+                                    flight['flightOriginDay'],
+                                    flight['numericFlightDate'],
+                                    flight['numGMTDate'],
+                                    flight['STDudt'],
+                                    flight['STAudt'],
+                                    flight['tailNumber'],
+                                    flight['numLastDateModified'],
+                                    flight['flightStatus'],
+                                    flight['origin'],
+                                    flight['STDLocal'],
+                                    flight['dispatchDesk'],
+                                    flight['STDGMTVariance'],
+                                    flight['destination'],
+                                    flight['STALocal'],
+                                    flight['STAGMTVariance'],
+                                    flight['OAGEquipmentType'],
+                                    flight['ACConfiguration'],
+                                    flight['serviceType'],
+                                    flight['originGate'],
+                                    flight['ETDlocal'],
+                                    flight['ETDudt'],
+                                    flight['TAXIutc'],
+                                    flight['OUTudt'],
+                                    flight['OFFudt'],
+                                    flight['destinationGate'],
+                                    flight['ETAlocal'],
+                                    flight['ETAudt'],
+                                    flight['ONudt'],
+                                    flight['INudt'],
+                                    flight['previousTailNumber'],
+                                    flight['ETE'],
+                                    flight['DCNutc'],
+                                    flight['ETOutc'],
+                                    flight['EONutc'],
+                                    flight['EDTCutc'],
+                                    flight['flightType'],
+                                    flight['newDepartureCity'],
+                                    flight['newArrivalCity'],
+                                    flight['SchedOAGEquipType'],
+                                    flight['OAGEquipSubType'],
+                                    flight['csvFSDailyID'],
+                                    flight['tailNumBeforeCancel'],
+                                    flight['CTAUTC'],
+                                    flight['cancelled'],
+                                    flight['replaced'],
+                                    flight['ATCStatus'],
+                                    flight['scheduledFlightType'],
+                                    flight['aircraftRouting'],
+                                    flight['mealService'],
+                                    flight['hub'],
+                                    flight['landingRestriction'],
+                                    flight['headStartFlight'],
+                                    flight['actualDeparture'],
+                                    flight['specialFlight'],
+                                    flight['actualArrival'],
+                                    flight['scheduledTaxiOut'],
+                                    flight['scheduledTaxiIn'],
+                                    flight['STOSetByUser'],
+                                    flight['STISetByUser'],
+                                    flight['CTFlightNumber']])
+                print('Finished filtering flights. Check filtered.csv')
 
     else:
         # if file is not available abort the script
@@ -182,156 +334,5 @@ check_if_folder_exist(stageReceive)
 
 call_adhoc_4_and_check_for_response_csv(stageReceive,stageSend,currentDirectory,currentDate)
 
-with open('DATA/data.json') as f:
-    data = json.load(f)
-filteredData = []
-for x in data:
-    if (x['STDudt'].strip() < x['ETDudt'].strip() 
-    and x['OUTudt'].strip() != '' 
-    and x['OFFudt'].strip() != ''
-    and x['ONudt'].strip() != ''
-    and x['INudt'].strip() != ''
-    and x['tailNumber'].strip().startswith('-') == False):
-        filteredData.append(x)
 
-
-with open('filtered_data/filtered.csv', 'w+', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(['recordStatus',
-                    'lastDateModified',
-                    'lastTimeModified',
-                    'lastUserToModify',
-                    'legDepartureDate',
-                    'airlineCode',
-                    'identifier',
-                    'sequence',
-                    'flightOriginDay',
-                    'numericFlightDate',
-                    'numGMTDate',
-                    'STDudt',
-                    'STAudt',
-                    'tailNumber',
-                    'numLastDateModified',
-                    'flightStatus',
-                    'origin',
-                    'STDLocal',
-                    'dispatchDesk',
-                    'STDGMTVariance',
-                    'destination',
-                    'STALocal',
-                    'STAGMTVariance',
-                    'OAGEquipmentType',
-                    'ACConfiguration',
-                    'serviceType',
-                    'originGate',
-                    'ETDlocal',
-                    'ETDudt',
-                    'TAXIutc',
-                    'OUTudt',
-                    'OFFudt',
-                    'destinationGate',
-                    'ETAlocal',
-                    'ETAudt',
-                    'ONudt',
-                    'INudt',
-                    'previousTailNumber',
-                    'ETE',
-                    'DCNutc',
-                    'ETOutc',
-                    'EONutc',
-                    'EDTCutc',
-                    'flightType',
-                    'newDepartureCity',
-                    'newArrivalCity',
-                    'SchedOAGEquipType',
-                    'OAGEquipSubType',
-                    'csvFSDailyID',
-                    'tailNumBeforeCancel',
-                    'CTAUTC',
-                    'cancelled',
-                    'replaced',
-                    'ATCStatus',
-                    'scheduledFlightType',
-                    'aircraftRouting',
-                    'mealService',
-                    'hub',
-                    'landingRestriction',
-                    'headStartFlight',
-                    'actualDeparture',
-                    'specialFlight',
-                    'actualArrival',
-                    'scheduledTaxiOut',
-                    'scheduledTaxiIn',
-                    'STOSetByUser',
-                    'STISetByUser',
-                    'CTFlightNumber'])
-    for flight in filteredData:
-        writer.writerow([flight['recordStatus'],
-                        flight['lastDateModified'],
-                        flight['lastTimeModified'],
-                        flight['lastUserToModify'],
-                        flight['legDepartureDate'],
-                        flight['airlineCode'],
-                        flight['identifier'],
-                        flight['sequence'],
-                        flight['flightOriginDay'],
-                        flight['numericFlightDate'],
-                        flight['numGMTDate'],
-                        flight['STDudt'],
-                        flight['STAudt'],
-                        flight['tailNumber'],
-                        flight['numLastDateModified'],
-                        flight['flightStatus'],
-                        flight['origin'],
-                        flight['STDLocal'],
-                        flight['dispatchDesk'],
-                        flight['STDGMTVariance'],
-                        flight['destination'],
-                        flight['STALocal'],
-                        flight['STAGMTVariance'],
-                        flight['OAGEquipmentType'],
-                        flight['ACConfiguration'],
-                        flight['serviceType'],
-                        flight['originGate'],
-                        flight['ETDlocal'],
-                        flight['ETDudt'],
-                        flight['TAXIutc'],
-                        flight['OUTudt'],
-                        flight['OFFudt'],
-                        flight['destinationGate'],
-                        flight['ETAlocal'],
-                        flight['ETAudt'],
-                        flight['ONudt'],
-                        flight['INudt'],
-                        flight['previousTailNumber'],
-                        flight['ETE'],
-                        flight['DCNutc'],
-                        flight['ETOutc'],
-                        flight['EONutc'],
-                        flight['EDTCutc'],
-                        flight['flightType'],
-                        flight['newDepartureCity'],
-                        flight['newArrivalCity'],
-                        flight['SchedOAGEquipType'],
-                        flight['OAGEquipSubType'],
-                        flight['csvFSDailyID'],
-                        flight['tailNumBeforeCancel'],
-                        flight['CTAUTC'],
-                        flight['cancelled'],
-                        flight['replaced'],
-                        flight['ATCStatus'],
-                        flight['scheduledFlightType'],
-                        flight['aircraftRouting'],
-                        flight['mealService'],
-                        flight['hub'],
-                        flight['landingRestriction'],
-                        flight['headStartFlight'],
-                        flight['actualDeparture'],
-                        flight['specialFlight'],
-                        flight['actualArrival'],
-                        flight['scheduledTaxiOut'],
-                        flight['scheduledTaxiIn'],
-                        flight['STOSetByUser'],
-                        flight['STISetByUser'],
-                        flight['CTFlightNumber']])
 
